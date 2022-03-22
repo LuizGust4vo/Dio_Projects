062cd07bb7ba4baf592f914637d175ea559b4c16
Luiz Gustavo <62315709+LuizGust4vo@users.noreply.github.com>
2022-03-22 13:28:06 -0300

    Add files via upload

---
+++ Sintaxe Básica em JavaScript/Aula 5 - Funções e suas Particularidades/Script.js
@@ -0,0 +1,16 @@
+ // alert ("Estudo de Funções"); 
+
+// Função:
+ function FunctionName (){
+    console.log ("Função Funcionando Perfeitamente!");
+ }
+
+ FunctionName ();
+
+
+ // Função com Parâmetros:
+ function Fparametro (V1, V2, V3) {
+     console.log (V1, V2, V3);
+ }
+
+ Fparametro ("Valor 1,", "Valor 2", "e Valor 3");
